b697e17e367ba2e6dbc634b00a599ed9e6a44f60
Moritz Zimmer <moritzzimmer@users.noreply.github.com>
2020-09-25 11:05:01 +0200

    made data sources of event_source_mappings conditional so they are not used for other event types (#6)

--- modules/event/dynamodb/main.tf
+++ modules/event/dynamodb/main.tf
@@ -1,4 +1,5 @@
 data "aws_region" "current" {
+  count = var.enable ? 1 : 0
 }
 
 resource "aws_lambda_event_source_mapping" "stream_source" {
@@ -12,6 +13,8 @@ resource "aws_lambda_event_source_mapping" "stream_source" {
 
 // see https://github.com/awslabs/serverless-application-model/blob/develop/samtranslator/policy_templates_data/policy_templates.json
 data "aws_iam_policy_document" "stream_policy_document" {
+  count = var.enable ? 1 : 0
+
   statement {
     actions = [
       "dynamodb:DescribeStream",
@@ -27,9 +30,9 @@ data "aws_iam_policy_document" "stream_policy_document" {
 
 resource "aws_iam_policy" "stream_policy" {
   count       = var.enable ? 1 : 0
-  name        = "${var.function_name}-stream-consumer-${data.aws_region.current.name}"
+  name        = "${var.function_name}-stream-consumer-${data.aws_region.current[count.index].name}"
   description = "Provides minimum DynamoDb stream processing permissions for ${var.function_name}."
-  policy      = data.aws_iam_policy_document.stream_policy_document.json
+  policy      = data.aws_iam_policy_document.stream_policy_document[count.index].json
 }
 
 resource "aws_iam_role_policy_attachment" "stream_policy_attachment" {
