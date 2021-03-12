85560baf18b96f8c1c738dc3fea2f0427cb7fd29
Moritz Zimmer <moritz.zmmr@gmail.com>
2021-03-12 11:26:19 +0100

    added support for CloudWatch Lambda Insights

--- modules/lambda/iam.tf
+++ modules/lambda/iam.tf
@@ -17,20 +17,27 @@ resource "aws_iam_role" "lambda" {
 }
 
 resource "aws_iam_role_policy_attachment" "cloudwatch_logs" {
-  role       = aws_iam_role.lambda.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda.name
 }
 
 resource "aws_iam_role_policy_attachment" "vpc_attachment" {
   count = var.vpc_config == null ? 0 : 1
 
-  role       = aws_iam_role.lambda.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+  role       = aws_iam_role.lambda.name
 }
 
 resource "aws_iam_role_policy_attachment" "tracing_attachment" {
   count = var.tracing_config_mode == null ? 0 : 1
 
-  role       = aws_iam_role.lambda.name
   policy_arn = "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess"
+  role       = aws_iam_role.lambda.name
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_lambda_insights" {
+  count = var.cloudwatch_lambda_insights_enabled ? 1 : 0
+
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLambdaInsightsExecutionRolePolicy"
+  role       = aws_iam_role.lambda.name
 }
