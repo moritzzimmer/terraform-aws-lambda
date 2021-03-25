7a7cdd510f35c761147cfe323fac4b2e087faa6e
Moritz Zimmer <moritzzimmer@users.noreply.github.com>
2021-03-25 20:21:59 +0100

    refactor: Removed sub-modules (#33)
    * added tests for lambda function
    * removed deprecated sub-modules
    
    fixes #14
    fixes #32
    
    BREAKING CHANGE: `terraform will recreated existing Lambda resources
    when appying this change

--- sns_subscriptions.tf
+++ sns_subscriptions.tf
@@ -2,16 +2,16 @@ resource "aws_lambda_permission" "sns" {
   for_each = var.sns_subscriptions
 
   action        = "lambda:InvokeFunction"
-  function_name = module.lambda.function_name
+  function_name = var.function_name
   principal     = "sns.amazonaws.com"
-  qualifier     = contains(keys(each.value), "endpoint") ? trimprefix(lookup(each.value, "endpoint"), "${module.lambda.arn}:") : null
+  qualifier     = contains(keys(each.value), "endpoint") ? trimprefix(lookup(each.value, "endpoint"), "${local.function_arn}:") : null
   source_arn    = lookup(each.value, "topic_arn")
 }
 
 resource "aws_sns_topic_subscription" "subscription" {
   for_each = var.sns_subscriptions
 
-  endpoint  = lookup(each.value, "endpoint", module.lambda.arn)
+  endpoint  = lookup(each.value, "endpoint", local.function_arn)
   protocol  = "lambda"
   topic_arn = lookup(each.value, "topic_arn")
 }
