cb7f59633d3ebb822819e41dc22be214eebd9385
Moritz Zimmer <moritz.zmmr@gmail.com>
2021-06-04 15:25:54 +0200

    refactor: removed workaround of lambda permission for aws provider < 3
    fixes #22

--- main.tf
+++ main.tf
@@ -144,10 +144,7 @@ resource "aws_lambda_permission" "cloudwatch_logs" {
   action        = "lambda:InvokeFunction"
   function_name = var.logfilter_destination_arn
   principal     = "logs.${data.aws_region.current.name}.amazonaws.com"
-  // workaround for https://github.com/terraform-providers/terraform-provider-aws/issues/14630
-  // in aws provider 3.x 'aws_cloudwatch_log_group.lambda.arn' interpolates to something like 'arn:aws:logs:eu-west-1:000000000000:log-group:/aws/lambda/my-group'
-  // but we need 'arn:aws:logs:eu-west-1:000000000000:log-group:/aws/lambda/my-group:*'
-  source_arn = length(regexall(":\\*$", aws_cloudwatch_log_group.lambda.arn)) == 1 ? aws_cloudwatch_log_group.lambda.arn : "${aws_cloudwatch_log_group.lambda.arn}:*"
+  source_arn    = "${aws_cloudwatch_log_group.lambda.arn}:*"
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "cloudwatch_logs_to_es" {
