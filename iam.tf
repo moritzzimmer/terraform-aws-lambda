c773fd35f18aa238184e7f6e7320cc584730fbf7
Michael Munsch <1969695+Mi3-14159@users.noreply.github.com>
2023-05-16 13:13:07 +0200

    feat: permit lambda write access only to it's own logs (#95)
    Co-authored-by: Moritz Zimmer <moritzzimmer@users.noreply.github.com>

--- iam.tf
+++ iam.tf
@@ -18,14 +18,6 @@ resource "aws_iam_role" "lambda" {
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
 
-
-resource "aws_iam_role_policy_attachment" "cloudwatch_logs" {
-  count = var.cloudwatch_logs_enabled ? 1 : 0
-
-  policy_arn = "arn:${data.aws_partition.current.partition}:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-  role       = aws_iam_role.lambda.name
-}
-
 resource "aws_iam_role_policy_attachment" "vpc_attachment" {
   count = var.vpc_config == null ? 0 : 1
 
@@ -73,5 +65,37 @@ resource "aws_iam_role_policy_attachment" "ssm" {
   count = try((var.ssm != null && length(var.ssm.parameter_names) > 0), false) ? 1 : 0
 
   policy_arn = aws_iam_policy.ssm[count.index].arn
+  role       = aws_iam_role.lambda.name
+}
+
+data "aws_iam_policy_document" "logs" {
+  count = var.cloudwatch_logs_enabled ? 1 : 0
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = [
+      "${aws_cloudwatch_log_group.lambda.arn}:*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "logs" {
+  count = var.cloudwatch_logs_enabled ? 1 : 0
+
+  description = "Provides minimum CloudWatch Logs write permissions."
+  name        = "${var.function_name}-logs-${data.aws_region.current.name}"
+  policy      = data.aws_iam_policy_document.logs[count.index].json
+}
+
+resource "aws_iam_role_policy_attachment" "logs" {
+  count = var.cloudwatch_logs_enabled ? 1 : 0
+
+  policy_arn = aws_iam_policy.logs[count.index].arn
   role       = aws_iam_role.lambda.name
 }
